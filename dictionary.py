8b73bf27e21483002462f4958a470fa9985e4964
YuvaVarshith4 <yuvavarshith4926@gmail.com>
2025-08-19 23:12:45 +0530

    random1

--- dictionary.py
+++ dictionary.py
@@ -1,12 +1,12 @@
-StudentGrades = {}
+Student_Grades = {}
 
 Tick = False
 while not Tick:
     name  = input("Enter your name: ")
     grade = float(input("Enter your grade: "))
-    StudentGrades[name] =  grade
+    Student_Grades[name] =  grade
     print(f"Student {name} added successfully")
-    print(StudentGrades)
+    print(Student_Grades)
     Ques = input("Do you want to continue adding? Y/N: ").lower()
     if Ques == "y":
         pass
