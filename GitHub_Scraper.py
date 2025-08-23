61fbaab301970f140208b691a8c5264bf24d0593
YuvaVarshith4 <yuvavarshith4926@gmail.com>
2025-08-23 23:56:59 +0530

    Github Scraper

--- GitHub_Scraper.py
+++ GitHub_Scraper.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+import time
 
 # ------------------------------------------------------------------------------------------------------------
 
@@ -22,11 +23,22 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 cdp = "/home/yuva/Downloads/chromedriver-linux64/chromedriver" 
 s = Service(executable_path=cdp)
-driver = webdriver.Chrome(service=s)
-# driver.get("https://www.google.com")
-driver.get("https://www.amazon.in/Lifelong-Dumbbells-Equipment-Exercise-Warranty/dp/B09W5PSTBP/ref=zg_bs_c_sports_d_sccl_1/258-7507423-9619246?pd_rd_w=lsALK&content-id=amzn1.sym.7f3d66f6-5df6-41bc-b3bc-9782a34ce834&pf_rd_p=7f3d66f6-5df6-41bc-b3bc-9782a34ce834&pf_rd_r=M31N9YW7NZNKQCER7CR8&pd_rd_wg=bLXId&pd_rd_r=c9ffa48e-bfcf-40f1-a7f7-3cd7c29c6020&pd_rd_i=B09W5PSTBP&th=1")
-price =  driver.find_element(By.CLASS_NAME, "a-price-whole")
-print(price.text)
-driver.quit()
+def check_price():
+    driver = webdriver.Chrome(service=s)
+    # driver.get("https://www.google.com")
+    try:
+        time.sleep(5)
+        driver.get("https://www.amazon.in/Lifelong-Dumbbells-Equipment-Exercise-Warranty/dp/B09W5PSTBP/ref=zg_bs_c_sports_d_sccl_1/258-7507423-9619246?pd_rd_w=lsALK&content-id=amzn1.sym.7f3d66f6-5df6-41bc-b3bc-9782a34ce834&pf_rd_p=7f3d66f6-5df6-41bc-b3bc-9782a34ce834&pf_rd_r=M31N9YW7NZNKQCER7CR8&pd_rd_wg=bLXId&pd_rd_r=c9ffa48e-bfcf-40f1-a7f7-3cd7c29c6020&pd_rd_i=B09W5PSTBP&th=1")
+        price =  driver.find_element(By.CLASS_NAME, "a-price-whole")
+        print(price.text)
+    except Exception as e:
+        print("Error:", e)
+    finally:
+        driver.quit()
+
+on = True
+while on:
+    check_price()
+    # break         
 
 
