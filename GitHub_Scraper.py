50cfbf08262f3fd67012d28d51cfa667b1000c1c
YuvaVarshith4 <yuvavarshith4926@gmail.com>
2025-08-24 00:03:52 +0530

    GitHub Scraper

--- GitHub_Scraper.py
+++ GitHub_Scraper.py
@@ -20,25 +20,19 @@ import time
 # ------------------------------------------------------------------------------------------------------------
 
 # Chrome will open and shutdown within seconds 
-
-cdp = "/home/yuva/Downloads/chromedriver-linux64/chromedriver" 
-s = Service(executable_path=cdp)
-def check_price():
-    driver = webdriver.Chrome(service=s)
-    # driver.get("https://www.google.com")
-    try:
+on = True
+while on:
+    def five_seconds():
         time.sleep(5)
+        cdp = "/home/yuva/Downloads/chromedriver-linux64/chromedriver" 
+        s = Service(executable_path=cdp)
+        driver = webdriver.Chrome(service=s)
+        # driver.get("https://www.google.com")
         driver.get("https://www.amazon.in/Lifelong-Dumbbells-Equipment-Exercise-Warranty/dp/B09W5PSTBP/ref=zg_bs_c_sports_d_sccl_1/258-7507423-9619246?pd_rd_w=lsALK&content-id=amzn1.sym.7f3d66f6-5df6-41bc-b3bc-9782a34ce834&pf_rd_p=7f3d66f6-5df6-41bc-b3bc-9782a34ce834&pf_rd_r=M31N9YW7NZNKQCER7CR8&pd_rd_wg=bLXId&pd_rd_r=c9ffa48e-bfcf-40f1-a7f7-3cd7c29c6020&pd_rd_i=B09W5PSTBP&th=1")
         price =  driver.find_element(By.CLASS_NAME, "a-price-whole")
         print(price.text)
-    except Exception as e:
-        print("Error:", e)
-    finally:
         driver.quit()
 
-on = True
-while on:
-    check_price()
-    # break         
+    five_seconds()
 
 
